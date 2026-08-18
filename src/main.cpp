6dd671dde4477bdb3ac15c9b4f5f7a8f5e40cfbd
yuutsu <nurikbaidibek@gmail.com>
2026-08-19 01:12:08 +0500

    feat: add update function in car

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,13 @@ constexpr int SCREEN_WIDTH = 800;
 constexpr int SCREEN_HEIGHT = 800;
 
 float to_radians(float deg) {
-    constexpr double df = 3.1415926f / 180.0f;
+    constexpr double df = PI / 180.0f;
     return deg * df;
+}
+
+float to_deg(float rad) {
+    constexpr double df = (180.0f / PI);
+    return rad * df;
 }
 
 Vector2 get_direction_from_angle(float angle) {
@@ -24,7 +29,7 @@ public:
     Vector2 position;
     Vector2 size;
 
-    float acceleration {20.0f};
+    float acceleration {100.0f};
     float speed {0.0f};
     float turn_speed {90.0f};
     float steering_angle {0.0f};
@@ -34,10 +39,24 @@ public:
 
     }
     void update(float delta_time) {
+        if (angle >= 360.0f)
+            angle -= 360.0f;
+        if (angle < 0.0f)
+            angle += 360.0f;
 
-
+        if (this->speed != 0.0f) {
+            float steering_radius {this->size.x / std::tanf(to_radians(this->steering_angle))};
+            float angular_velocity {this->speed / steering_radius };
+            Vector2 direction = get_direction_from_angle(this->angle);
+            this->position.x += this->speed * direction.x * delta_time;
+            this->position.y += this->speed * direction.y * delta_time;
+            this->angle += to_deg(angular_velocity) * delta_time;
+        }
     }
     void gas(float hw,float delta_time) {
+        float input = std::clamp(hw,-1.0f,1.0f);
+        this->speed = std::min(100.0f,this->speed + acceleration * input * delta_time);
+
     }
     void turn(float hw,float delta_time) {
         float input = std::clamp(hw,-1.0f,1.0f);
@@ -57,6 +76,10 @@ public:
             //rudder
             Vector2 rudder {get_direction_from_angle(car.angle + car.steering_angle)};
             DrawLine(end_pos.x,end_pos.y ,end_pos.x + rudder.x * 30,end_pos.y + rudder.y * 30,PURPLE);
+
+
+            float steering_radius {car.size.x / std::tanf(to_radians(car.steering_angle))};
+            DrawCircleLines(car.position.x + car_direction.y * steering_radius,car.position.y + -(car_direction.x) * steering_radius,steering_radius,BLACK);
         }
     }
 };
@@ -77,11 +100,10 @@ int main()
         float delta_time = GetFrameTime();
         if (IsKeyDown(KEY_S)) main_car.gas(-0.5f,delta_time);
         else if (IsKeyDown(KEY_W)) main_car.gas(0.5f,delta_time);
-        if (IsKeyDown(KEY_D)) main_car.turn(-0.5f,delta_time);
-        else if (IsKeyDown(KEY_A)) main_car.turn(0.5f,delta_time);
+        if (IsKeyDown(KEY_D)) main_car.turn(-0.9f,delta_time);
+        else if (IsKeyDown(KEY_A)) main_car.turn(0.9f,delta_time);
 
         main_car.update(delta_time);
-        main_car.angle += 0.3f;
         BeginDrawing();
 
         ClearBackground(DARKGREEN);
@@ -90,6 +112,7 @@ int main()
         DrawText(TextFormat("Frame time: %02.02f ms", GetFrameTime()), 10, 10, 20, WHITE);
         DrawText(TextFormat("Fps: %i", GetFPS()), 10, 30, 20, WHITE);
         DrawText(TextFormat("ANGLE: %f", main_car.angle), 10, 50, 20, WHITE);
+        DrawText(TextFormat("SPEED: %f", main_car.speed), 10, 70, 20, WHITE);
         EndDrawing();
 
 
